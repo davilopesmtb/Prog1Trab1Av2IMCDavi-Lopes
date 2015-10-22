c7b0051ff1953926d931d7707b4542db3e6ae3d9
Davi Lopes Dos Santos <davilopes_@hotmail.com>
2015-10-21 23:31:22 -0200

    atualizacao do trab

--- imc.c
+++ imc.c
@@ -8,7 +8,7 @@ int main()
     printf("Informe o seu peso (em kg):\n"); //inserindo peso
     scanf("%f", &peso);
     
-    printf("Informe a sua altura (em metros):\n"); //inserindo altura
+    printf("Informe a sua altura (ex: 1.85):\n"); //inserindo altura
     scanf("%f", &altura);
     
     imc = peso / (altura*altura); //respostas de cada resultado
@@ -16,15 +16,18 @@ int main()
         printf("Idice de Massa Corporal igual a: %.2f Você esta magra (o)\n", imc );
          }
                 
-        if (imc >=20 &&  imc<25){
+        if (imc >=20 &&  imc<25)
+         {
          printf ("Indice de Massa Corporal igual a: %.2f Você esta com o peso ideal\n", imc);
          }
 
-        if (imc >=26 &&  imc<30){
+        if (imc >=26 &&  imc<30)
+         {
          printf ("Indice de Massa Corporal igual a: %.2f Você esta acima do peso\n", imc);
          }
             
-        if (imc >=31){
+        if (imc >=31)
+         {
          printf("Indice de Massa Corporal igual a: %.2f Você esta obeso (a). Sendo essa situação prejudicial para sua saúde.\n", imc);
          }
 
